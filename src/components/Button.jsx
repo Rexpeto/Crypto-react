7e9233eaa40aca3c6f80213f7efed8d85fb301a2
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 04:24:04 -0400

    Componente de boton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-const Button = () => {
-    return (
-        <div>Button</div>
-    );
-};
+const Button = ({styles}) => (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient ${styles} font-poppins font-medium text-[18px] text-primary outline-none`}>Empezar</button>
+);
 
 export default Button;
