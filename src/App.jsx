0d2dc778f044ffc57ee9f7a1db265326251dc95c
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 00:00:45 -0400

    Importaciones y estructura inicial de los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import React from "react"
+import styles from "./style";
+
+import {
+    NavBar,
+    Button,
+    Clientes,
+    Factura,
+    Estadistica,
+    CTA,
+    FeedbackCard,
+    CardDeal,
+    GetStarted,
+    Hero,
+    Negocios,
+    Testimonios,
+    Tratos,
+    Footer,
+} from "./components";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <div className="bg-primary w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <NavBar />
+                </div>
+            </div>
+            //* Hero
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Hero />
+                </div>
+            </div>
+            //* All
+            <div
+                className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}
+            >
+                <div className={`${styles.boxWidth}`}>
+                    <Estadistica/>
+                    <Negocios/>
+                    <Factura/>
+                    <CardDeal/>
+                    <Testimonios/>
+                    <CTA/>
+                    <Footer/>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default App
+export default App;
