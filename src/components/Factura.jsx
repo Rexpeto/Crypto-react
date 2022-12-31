09224c5593907915edfe12f0bddc4b76016b72a6
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 22:21:34 -0400

    se deshabilitó los gradiantes

--- src/components/Factura.jsx
+++ src/components/Factura.jsx
@@ -6,8 +6,8 @@ const Factura = () => (
         <div className={layout.sectionImgReverse}>
             <img src={bill} alt="Facturación" className="w-[100%] h-[100%] relative z-[5]" />
 
-            <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
-            <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
+            {/* <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
+            <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div> */}
         </div>
 
         <div className={layout.sectionInfo}>
