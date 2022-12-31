dcf499192365876d76026e617ccc4d8cf8f5f2b4
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 21:58:23 -0400

    Se agregó el id

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -2,7 +2,7 @@ import {clients} from '../constants';
 import styles from '../style';
 
 const Clientes = () => (
-    <section className={`${styles.flexCenter} my-4`}>
+    <section className={`${styles.flexCenter} my-4`} id="clients">
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
             {clients.map(client => (
                 <div className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`} key={client.id}>
