b9966159288e9474b834002ffeaa81b04be6bc29
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 20:05:24 -0400

    bordes redondeados

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({styles}) => (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient ${styles} font-poppins font-medium text-[18px] text-primary outline-none`}>Empezar</button>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient ${styles} font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px]`}>Empezar</button>
 );
 
 export default Button;
