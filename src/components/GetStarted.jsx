1714fc68efc82045cfce76147e957ed7fcf63796
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-30 22:03:33 -0400

    Se agregó animación

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,12 +3,12 @@ import { arrowUp } from '../assets';
 
 const GetStarted = () => (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full getBtn transition-all`}>
             <div className={`${styles.flexStart} flex-row`}>
                 <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
                     <span className='text-gradient'>Empezar</span>
                 </p>
-                    <img src={arrowUp} alt="arrowUp" className='w-[23px] h-[23px] object-contain' />
+                    <img src={arrowUp} alt="arrowUp" className='w-[23px] h-[23px] object-contain transition-all' />
             </div>
         </div>
     </div>
